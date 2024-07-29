d9293f3fd02d24ae2f051bb166ce6df1871ee1bc
miguelarian <miguelvr88@gmail.com>
2024-07-29 18:15:48 +0200

    add tests for size < 3

--- tests/spiral.test.js
+++ tests/spiral.test.js
@@ -1,3 +1,15 @@
-test('assert true', () => {
-    expect(true).toBe(true)
+const spiral = require('../src/spiral')
+
+describe('spiral of less than 3', () => {
+    it('should throw an error for 0', () => {
+        expect(() => spiral(0)).toThrowError('Size must be odd number >= 3')
+    })
+
+    it('should throw an error for 1', () => {
+        expect(() => spiral(1)).toThrowError('Size must be odd number >= 3')
+    })
+
+    it('should throw an error for 2', () => {
+        expect(() => spiral(2)).toThrowError('Size must be odd number >= 3')
+    })
 })
